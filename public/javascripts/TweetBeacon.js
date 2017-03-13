3f11e54c76b8c5598507c07a092cb955e6b36e11
Magda Varvouzou <magdoula29v@hotmail.com>
2017-03-13 13:30:48 +0200

    colors +-
    colors +--

--- public/javascripts/TweetBeacon.js
+++ public/javascripts/TweetBeacon.js
@@ -17,11 +17,17 @@ TweetBeacon = function(tweet) {
   // Set base color depending on sentiment score
   this.color = 0xFFFFFF;
 
-  if (tweet.sentiment.score < 0) {
+   if (tweet.sentiment.score <= 0 && tweet.sentiment.score <= -2 ) {
+  this.color = 0xdddf10;
+  }
+  if (tweet.sentiment.score <= -3 && tweet.sentiment.score <= -5 ) {
   this.color = 0xFF0000;
   }
-  else if (tweet.sentiment.score > 0) {
-    this.color = 0xDDDD00;
+  else if (tweet.sentiment.score > 0 && tweet.sentiment.score < 3) {
+    this.color = 0x39b62d;
+  }
+  else if (tweet.sentiment.score >= 3 && tweet.sentiment.score <= 5) {
+    this.color = 0x0b3ec9;
   }
 
   this.addBeam();
