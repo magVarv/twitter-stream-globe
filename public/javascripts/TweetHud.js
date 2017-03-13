a9606a18ed76741f34a04537d09c05088fb3b373
Magda Varvouzou <magdoula29v@hotmail.com>
2017-03-13 15:48:36 +0200

    sentiment change

--- public/javascripts/TweetHud.js
+++ public/javascripts/TweetHud.js
@@ -92,11 +92,17 @@ app.controller('TweetHud', function($scope, $resource, $timeout, $rootScope, $ti
 		
 		var state = 'neutral';
 
-  	if (score < 0) {
-  		state = 'negative';
+  	if (score <= 0 &&  score>=-1) {
+  		state = 'negative2';
   	}
-  	else if (score > 0) {
-  		state = 'positive';
+	else if (score <= -2 && score >= -5) {
+  		state = 'negative1';
+  	}
+	else if (score > 0 && score <= 1) {
+  		state = 'positive1';
+  	}
+  	else if (score >= 2 && score <= 5) {
+  		state = 'positive2';
   	}
 
 		return state;
