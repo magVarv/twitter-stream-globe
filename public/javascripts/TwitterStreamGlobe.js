be9df2628b8148b8e8eff75d8b357303d51cae7f
Magda Varvouzou <magdoula29v@hotmail.com>
2017-05-08 16:12:15 +0300

    final speed earth

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -163,7 +163,7 @@
 	 */ 
 	function render () {
 
-		earthMesh.rotation.y = earthMesh.rotation.y + 10000000;
+		earthMesh.rotation.y = earthMesh.rotation.y + 50;
 		
 	  renderer.autoClear = false;
 	  renderer.clear();
