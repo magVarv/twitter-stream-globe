8c23be5d2f29b32a6e5cc3016cb434ef791fb43a
Magda Varvouzou <magdoula29v@hotmail.com>
2017-06-05 22:39:35 +0300

    test geometry 2
    yolo

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -53,7 +53,7 @@
 	 */
 	function addEarth () {
 
-	  var sphereGeometry = new THREE.SphereGeometry(600, 50);
+	  var sphereGeometry = new THREE.SphereGeometry(600);
 
 	  var shader = Shaders.earth;
 	  var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
