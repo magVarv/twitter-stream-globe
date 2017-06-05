f828f7f83d4bfdea5138afef0b191af8780db302
Magda Varvouzou <magdoula29v@hotmail.com>
2017-06-06 00:33:21 +0300

    0 rotation

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -166,7 +166,7 @@
 	 */ 
 	function render () {
 
-		earthMesh.rotation.y = earthMesh.rotation.y + 0.001;
+		earthMesh.rotation.y = earthMesh.rotation.y + 0;
 		
 	  renderer.autoClear = false;
 	  renderer.clear();
