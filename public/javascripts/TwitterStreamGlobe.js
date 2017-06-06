fd5a507cbbb08c41d77c9df2c44fae792ed3d546
Magda Varvouzou <magdoula29v@hotmail.com>
2017-06-06 13:57:39 +0300

    shpere geometry

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -53,7 +53,7 @@
 	 */
 	function addEarth () {
 
-	  var sphereGeometry = new THREE.PlaneGeometry(2000, 1800);
+	  var sphereGeometry = new THREE.sphereGeometry(600, 50, 50);
 
 	  var shader = Shaders.earth;
 	  var uniforms = THREE.UniformsUtils.clone(shader.uniforms);
